9687c84b2f0138180fff179908204ccc9fb768bb
Martin Gamboa <martingamboa2429@gmail.com>
2025-10-30 22:33:51 -0300

    Integracion de Dynamic Reports
    Se agregó el archivo xml de configuración default en src/main/resources/dynamicreports-defaults.xml
    Se agregó la configuración de maven en el archivo pom.xml

---
+++ backend/src/main/java/com/grindsup/backend/model/ReportTest.java
@@ -0,0 +1,14 @@
+// package com.grindsup.backend.model;
+// package net.sf.dynamicreports.examples.adhoc;
+
+// import static net.sf.dynamicreports.report.builder.DynamicReports.*;
+
+// public class ReportTest {
+//     private void build(){
+//         try{
+//             report()
+//             .columns
+//         }
+//     }
+    
+// }
